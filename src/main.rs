b2e2871ffc16ad6972ac23d2413add0334987657
Kevin Koshy <kevin_koshy@hotmail.com>
2022-06-21 13:58:01 +0530

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use std::io;
+
 fn main() {
-    let mut mars_weight:f32 = calculate_weight_on_mars(100.0);
-    mars_weight = mars_weight * 1000.0;
-    println!("The weight on mars is {} grams", mars_weight);
+    println!("Please enter your weight (kg): ");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight:f32 = input.trim().parse().unwrap();
+
+
+    let mars_weight:f32 = calculate_weight_on_mars(weight);
+    println!("The weight on mars is {} ", mars_weight);
 }
 
 fn calculate_weight_on_mars(weight:f32) -> f32 {
